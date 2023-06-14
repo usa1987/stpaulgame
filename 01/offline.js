76b9c2644db6f287657a4f3b374cd31929725024
usa1987 <136548490+usa1987@users.noreply.github.com>
2023-06-14 10:35:11 +0700

    01 upload

---
+++ 01/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1686713543,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/correct-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
